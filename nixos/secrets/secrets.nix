253df393a378b3b409bded00ff0ce0de4da55bba
creaaidev <martimrmoniz@gmail.com>
2023-02-19 11:12:41 +0000

    Fix typo2

--- nixos/secrets/secrets.nix
+++ nixos/secrets/secrets.nix
@@ -1,11 +1,11 @@
 let
   # system keys
-  tsukuyomi = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINC7qWv7Fjim/F3koJBBzJQA22obpXlU7nnTh0ymEZjm root@tsukuyomi" ];
+  tsukuyomi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINC7qWv7Fjim/F3koJBBzJQA22obpXlU7nnTh0ymEZjm root@tsukuyomi";
   systems = [ tsukuyomi ];
 
   # user keys
-  tsukuyomiUser = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILW5ZVdVaKMVlau1wp/JGJpdpE6JUxJ07DEYHi9qOLC8 crea@tsukuyomi" ];
-  ebisuUser = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcUMSSFZQheROdhFVmIUwBTbAVBv9YUm/Ib3ED3O0gv crea@pasokon" ];
+  tsukuyomiUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILW5ZVdVaKMVlau1wp/JGJpdpE6JUxJ07DEYHi9qOLC8 crea@tsukuyomi";
+  ebisuUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcUMSSFZQheROdhFVmIUwBTbAVBv9YUm/Ib3ED3O0gv crea@pasokon";
   users = [ tsukuyomiUser ebisuUser ];
 in
 {
